fdcd287b9dd152a93ee9828a9194321ba3ce49f2
SeoHyeonMyeong <gusaud2199@naver.com>
2022-02-15 17:07:11 +0900

    Update: styles of NavBar component

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "./NavBar.module.css";
 
@@ -6,26 +7,29 @@ export default function Home() {
     const router = useRouter();
     return (
         <nav className={styles.nav}>
-            <Link href="/">
-                <a 
-                    className={[
-                        styles.link,
-                        router.pathname === "/" ? styles.active : "",
-                    ].join(" ")}
-                >
-                    Home
-                </a>
-            </Link>
-            <Link href="/about">
-                <a 
-                    className={[
-                        styles.link,
-                        router.pathname === "/about" ? styles.active : "",
-                    ].join(" ")}
-                >
-                    About
-                </a>
-            </Link>
+            <Image alt="nav_image" src="/vercel.svg" width="100" height="60"/>
+            <div>
+                <Link href="/">
+                    <a 
+                        className={[
+                            styles.link,
+                            router.pathname === "/" ? styles.active : "",
+                        ].join(" ")}
+                    >
+                        Home
+                    </a>
+                </Link>
+                <Link href="/about">
+                    <a 
+                        className={[
+                            styles.link,
+                            router.pathname === "/about" ? styles.active : "",
+                        ].join(" ")}
+                    >
+                        About
+                    </a>
+                </Link>
+            </div>
         </nav>
     )
 }
